6c2ba5bca4727df480f521893cd7093a1421420c
Hink <CH049991@Cerner.net>
2022-02-01 16:47:00 -0600

    create workout form functionality

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-function nameWorkout() {
+const addWorkoutButton = document.querySelector("#submit");
+const workoutCard = document.querySelectorAll(".card");
+const formWrap = document.querySelector(".form-wrap");
+const createWorkout = document.querySelector("#create");
+const closeCreate = document.querySelector("#close");
+
+workoutCard.forEach((card) => {
+  card.addEventListener("click", (e) => {
+    event.preventDefault();
+    e.target.remove();
+  });
+});
+
+addWorkoutButton.addEventListener("click", () => {
   event.preventDefault();
-  let workoutName = document.getElementById("workout-name").value;
-  console.log(workoutName);
-}
+  let workoutName = document.querySelector("#workout-name").value;
+  let workoutCategory = document.querySelector("#workoutCategory").value;
+
+  console.log(workoutName, workoutCategory);
+});
+
+createWorkout.addEventListener("click", () => {
+  formWrap.style.display = "block";
+});
+
+closeCreate.addEventListener("click", () => {
+  formWrap.style.display = "none";
+});
